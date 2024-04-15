4e73daff6affc199949cd3ff24a2591266b44135
ethan bleier <ebleier4@gmail.com>
2024-04-14 19:34:56 -0700

    added comment to mainactivity

--- app/src/main/java/com/example/movie/MainActivity.java
+++ app/src/main/java/com/example/movie/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Move to user landing page activity
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
         final MovieListAdapter adapter = new MovieListAdapter(new MovieListAdapter.MovieDiff());
         recyclerView.setAdapter(adapter);
